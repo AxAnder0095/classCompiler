07c3160819559ffb62fe6a57b899272cfd020bb9
Frenguin <104327737+Frenguin@users.noreply.github.com>
2024-03-12 15:22:28 -0500

    added files to repo

---
+++ synAnalyzerMain.h
@@ -0,0 +1,14 @@
+//
+// Created by doris on 3/12/2024.
+//
+
+#ifndef CLASSCOMPILER_SYNANALYZERMAIN_H
+#define CLASSCOMPILER_SYNANALYZERMAIN_H
+
+
+class synAnalyzerMain {
+
+};
+
+
+#endif //CLASSCOMPILER_SYNANALYZERMAIN_H
